057dd3770beefaf1ae9dd1e27ad61ffd81635c39
 Andrey Smelov <xor29a@bk.ru>
2019-02-02 22:50:44 +0300

    Implement toggl websocket service

---
+++ src/service/ReconnectingWebsocket.ts
@@ -0,0 +1,49 @@
+export type WebsocketStatus = 'offline' | 'connecting' | 'online'
+
+abstract class ReconnectingWebsocket {
+  protected connectTimeout = 5000
+  protected defaultTimeout = 10000
+  protected timeout: number = this.defaultTimeout
+  protected timeoutFactor = 1.2
+
+  constructor(private url: string) {
+    this.url = url
+  }
+
+  public start(): void {
+    this.reconnect()
+  }
+
+  protected abstract connectCallback(socket: WebSocket): void
+  protected abstract messageCallback(socket: WebSocket, response: string): void
+  protected statusCallback(status: WebsocketStatus): void {} // tslint:disable-line:no-empty
+
+  protected reconnect(): void {
+    const socket = new WebSocket(this.url)
+    this.statusCallback('connecting')
+
+    const connectTimer = setTimeout(() => {
+      socket.close()
+    }, this.connectTimeout)
+
+    socket.onopen = (): void => {
+      clearTimeout(connectTimer)
+      this.statusCallback('online')
+      this.timeout = this.defaultTimeout
+      this.connectCallback(socket)
+    }
+
+    socket.onmessage = (response: MessageEvent): void => {
+      this.messageCallback(socket, response.data)
+    }
+
+    socket.onclose = (): void => {
+      clearTimeout(connectTimer)
+      this.statusCallback('offline')
+      setTimeout(() => this.reconnect(), this.timeout)
+      this.timeout *= this.timeoutFactor
+    }
+  }
+}
+
+export default ReconnectingWebsocket
---
+++ src/service/TogglWebsocketService.ts
@@ -0,0 +1,73 @@
+import ReconnectingWebsocket from '@/service/ReconnectingWebsocket'
+
+const TOGGL_WS_URL = 'wss://stream.toggl.com/ws'
+
+type TogglCallback = (event: TogglEvent) => void
+
+export interface TimeEntry {
+  id: number,
+  description: string,
+  start: string,
+  stop: string|null,
+  duration: number
+}
+
+export const enum TogglAction {
+  Update,
+  Delete,
+  Insert,
+}
+
+export interface TogglEvent {
+  action: TogglAction,
+  data: TimeEntry,
+}
+
+class TogglWebsocketService extends ReconnectingWebsocket {
+  constructor(private token: string, private callback: TogglCallback) {
+    super(TOGGL_WS_URL)
+  }
+
+  protected connectCallback(socket: WebSocket): void {
+    const message = {
+      type: 'authenticate',
+      api_token: this.token,
+    }
+
+    socket.send(JSON.stringify(message))
+  }
+
+  protected messageCallback(socket: WebSocket, response: string): void {
+    const data = JSON.parse(response)
+
+    if (data.model === undefined) {
+      socket.send('{"type": "pong"}')
+      return
+    }
+
+    if (data.model !== 'time_entry') {
+      return
+    }
+
+    data.action = this.convertAction(data.action)
+    this.callback(data)
+  }
+
+  protected convertAction(action: string): TogglAction {
+    if (action === 'UPDATE') {
+      return TogglAction.Update
+    }
+
+    if (action === 'delete') {
+      return TogglAction.Delete
+    }
+
+    if (action === 'INSERT') {
+      return TogglAction.Insert
+    }
+
+    throw Error('Unexpected action' + action)
+  }
+}
+
+export default TogglWebsocketService
